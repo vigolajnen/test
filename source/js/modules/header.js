55d0fe5b8310b969c5c1928d978dc51124c53be4
Vigolajnen Anna <vigolajnen@gmail.com>
2023-10-22 22:38:01 +0300

    Добавила информацию в подвал сайта + шапка fixed

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,2 +1,27 @@
-function toggleNavCity() { document.getElementById('nav-city').classList.toggle('hidden')
+const menu = document.querySelector('.menu');
+const menuBtn = menu.querySelector('#menu-button span');
+const menuList = menu.querySelector('#menu-list');
+
+function toggleNavCity() {
+  menuList.classList.toggle('hidden');
+}
+
+if (window.location.pathname.includes('spb')) {
+  menuBtn.innerText = 'Санкт-Петербург';
+} else {
+  menuBtn.innerText = 'Самара';
+}
+
+
+window.onscroll = function() {onHeaderSticky()};
+
+const header = document.querySelector("header");
+const sticky = header.offsetTop;
+
+function onHeaderSticky() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
 }
